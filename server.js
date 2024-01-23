0ca44b08f994c36a759787a7044df7619a0188b2
nbuzzerio <nick.buzzerio@hotmail.com>
2024-01-22 20:44:34 -0500

    added express

---
+++ server.js
@@ -0,0 +1,17 @@
+import express from 'express';
+
+const app = express();
+const port = process.env.PORT || 7000;
+
+// Serve static files from the 'dist' folder
+app.use(express.static('./dist'));
+
+// Define a catch-all route to serve 'index.html'
+app.get('*', (req, res) => {
+  res.sendFile(__dirname, './dist/index.html');
+});
+
+// Start the server
+app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
+});
